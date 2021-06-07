2b8bf71864af841e2904f4512ab79d39c91571b6
Mimi Vo <mimzy414@gmail.com>
2021-06-07 17:13:03 -0500

    added tables and data

--- db/schema.sql
+++ db/schema.sql
@@ -3,3 +3,18 @@ CREATE TABLE department (
   name VARCHAR(30) NOT NULL
 );
 
+CREATE TABLE role (
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(10, 2) NOT NULL,
+  department_id INT NOT NULL,
+  FOREIGN KEY (department_id)
+);
+
+CREATE TABLE employee (
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL
+  manager_id 
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,40 @@
-INSERT INTO department (id, name)
+INSERT INTO department (name)
 VALUES 
-(101, 'Kitchen'),
-(112, 'Management'),
-(204, 'Floor'),
-(315, 'Bar');
+('Kitchen'),
+('Management'),
+('Floor'),
+('Bar');
+
+
+INSERT INTO role (title, salary, department_id)
+VALUES 
+('Head Chef', 90000, 1),
+('Sous Chef', 82000, 1),
+('Founder', 160000, 2),
+('Restaurant Manager', 110000, 2),
+('Server', 57000, 3),
+('Hostess', 55500, 3),
+('Mixologist', 63000, 4);
+
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES 
+('Mimi', 'Vo', 3, 1),
+('Gordon', 'Ramsey', 2, 2),
+('Bobby', 'Flay', 6, 3),
+('David', 'Chang', 1, 2),
+('Anthony', 'Bordain', 2, 2),
+('Masahuru', 'Morimoto', 7, 3),
+('Eric', 'Ripert', 5, 3),
+('Aaron', 'Sanchez', 2, 2),
+('Andrew', 'Zimmern', 6, 3),
+('Julia', 'Child', 4, 1);
+
+
+
+INSERT INTO manager (first_name, last_name)
+('Mimi', 'Vo'),
+('David', 'Chang'),
+('Julia', 'Child');
+
